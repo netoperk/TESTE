50ffe5b5376671d01e94cff4bd516e929595ef29
netoperk <47359713+netoperk@users.noreply.github.com>
2020-08-11 21:19:46 -0600

    Practica de Vuex
    Practica de Vuex

---
+++ Vue/components/main.js
@@ -0,0 +1,24 @@
+Vue.component('main_component',{
+		template:`
+					<div>
+						<main_nav></main_nav>
+						<div id="container" style="margin-top:60px;">Usuario Logeado: {{log_user}}</div>
+						<button type="button" @click="aumentar" class="btn btn-info btn-sm">+</button>
+						<button type="button" @click="disminuir" class="btn btn-danger btn-sm">-</button>
+						<h3>{{numero}}</h3>
+						<button type="button" @click="obtenerDatos" class="btn btn-danger btn-sm">Obtener Datos</button>
+						<ul v-for="item of progs">
+							<li>{{item.id_programador}} - {{item.usuario}}</li>
+						</ul>
+					</div>
+				`,
+
+		computed:{
+			...Vuex.mapState(['log_user','numero','progs'])
+		},
+		methods:{
+			...Vuex.mapMutations(['aumentar','disminuir']),
+			...Vuex.mapActions(['obtenerDatos'])	
+		}
+
+	});
---
+++ Vue/main_vue.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+$_SESSION['usuario'] = "PEDRO";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>COMPONENTES DE VUE</title>
+	  <link rel="stylesheet" href="../libs/bootstrap-4.5.0/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="../libs/fontawesome-5.14.0/css/all.css">
+	  <script src="../libs/jquery-3.5.1/jquery-3.5.1.min.js"></script>	  
+	  <script src="../libs/bootstrap-4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div id="main_app">
+		<main_component></main_component>		
+	</div>
+</body>
+<script src="../libs/Vue/vue.js"></script>
+<script src="../libs/Vue/vuex.js"></script>
+<script src="components/main.js"></script>
+<script src="components/nav_bar.js"></script>
+<script>
+	const store = new Vuex.Store({
+		state:{
+			log_user:"<?php echo $_SESSION['usuario']; ?>",
+			numero:0,
+			progs:[]
+		},
+		mutations:{
+			aumentar(state){
+				state.numero++
+			},
+			disminuir(state){
+				state.numero--
+			},
+			llenarDatos(state,datosAccion){
+				state.progs = datosAccion
+			}
+		},
+		actions:{
+			obtenerDatos: async function({commit}){
+				const data = await fetch('../datos.json');
+				const progs = await data.json();
+				commit('llenarDatos',progs)
+			}
+		}
+	});
+	new Vue({
+		el:'#main_app',
+		store:store
+	});
+</script>
+</html>
